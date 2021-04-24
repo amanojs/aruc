3e68439adaffa74554f33363667b30df80fed7f8
takashiVue <takashivue@gmail.com>
2021-04-24 16:23:07 +0900

    commit

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -3,14 +3,20 @@ import logo from './logo.svg';
 import './App.css';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import UserList from './pages/UserList';
+import Noti from './pages/Noti'
 
 function App() {
   return (
     <div className="App">
+      <a href="/noti">通知画面</a>
+      <a href="/">探す</a>
       <Router>
         <Switch>
           <Route path="/" exact={true}>
             <UserList />
+          </Route>
+          <Route path="/noti" exact={true}>
+            <Noti />
           </Route>
         </Switch>
       </Router>
--- client/src/pages/Noti/index.tsx
+++ client/src/pages/Noti/index.tsx
@@ -37,7 +37,30 @@ class Noti extends React.Component<Props,State> {
 
     render(){
         return (
-            <div>Noti</div>
+            <div>
+                    <div>
+                        <h2>お知らせ</h2>
+                        <p className="h2sub">NEWS</p>
+                    </div>
+                    <div className="ikubox">
+                        <p className="ikuyoRequest">{this.state.notiList.length}件の「いくよ」リクエストが来ています</p>
+                        <div className="iku">
+                            <span id="date">2021/06/05</span>
+                            <span id="userName">田中</span>
+                            <button id="submit" onClick={() => this.acceptRequest()}>承認</button>
+                        </div>
+                    </div>
+
+                    <div className="kitebox">
+                        <p className="kiteRequest">n件の「いくよ」リクエストが来ています</p>
+                        <div className="iku">
+                            <span id="date">2021/06/05</span>
+                            <span id="userName">田中</span>
+                        </div>
+                    </div>
+                </div>
         );
     }
 }
+
+export default Noti;
